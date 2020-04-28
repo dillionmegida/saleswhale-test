80ef995e54793b8e06be2b0829b73eef9309f736
Dillion Megida <dillionmegida@gmail.com>
2020-04-29 00:40:26 +0100

    "chores: project setup"

---
+++ src/pages/home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./index.module.scss";
+
+const Home = () => {
+	return (
+		<main className={styles.main}>
+			<h1>Hompage : )</h1>
+		</main>
+	);
+};
+
+export default Home;
